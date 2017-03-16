6d8daa2e2852fe7493222eaa2ac5264acbfcc6b5
Miguel Salas <al286275@uji.es>
2017-03-16 20:05:38 +0100

    Improved SignUpRoute.post and its tests

--- src/main/java/com/vendingontime/backend/routes/SignUpRoute.java
+++ src/main/java/com/vendingontime/backend/routes/SignUpRoute.java
@@ -1,12 +1,16 @@
 package com.vendingontime.backend.routes;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.vendingontime.backend.models.Person;
 import com.vendingontime.backend.models.PersonCollisionException;
 import com.vendingontime.backend.models.bodymodels.SignUpData;
 import com.vendingontime.backend.repositories.PersonRepository;
 import com.vendingontime.backend.routes.utils.AppRoute;
 import com.vendingontime.backend.routes.utils.Response;
+
+import java.io.IOException;
 
 /**
  * Created by miguel on 13/3/17.
@@ -26,9 +30,12 @@ public class SignUpRoute {
             SignUpData personCandidate = mapper.readValue(requestBody, SignUpData.class);
 
             return createUser(personCandidate);
-        } catch (Exception ex) {
+        } catch (JsonMappingException ex) {
+            //FIXME Change cause?
+            return response.badRequest(ex.getCause());
+        } catch (IOException ex) {
             ex.printStackTrace();
-            //FIXME
+
             return null;
         }
     }
@@ -36,7 +43,7 @@ public class SignUpRoute {
     private AppRoute createUser(SignUpData personCandidate) {
         String[] signUpErrors = personCandidate.validate();
         if(signUpErrors.length != 0) {
-            response.badRequest(signUpErrors);
+            return response.badRequest(signUpErrors);
         }
 
         Person person = new Person(personCandidate);
@@ -44,8 +51,7 @@ public class SignUpRoute {
         try {
             repository.create(person);
         } catch (PersonCollisionException ex) {
-            // TODO Error
-            response.badRequest(ex.getCauses());
+            return response.badRequest(ex.getCauses());
         }
 
         return response.created(person);
