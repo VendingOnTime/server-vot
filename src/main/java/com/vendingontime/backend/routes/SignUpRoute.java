50b1d9470bffbd5ff0c4a65f7ed2c65356abb3dd
Miguel Salas <al286275@uji.es>
2017-03-16 18:35:28 +0100

    SignUpRoute first version

--- src/main/java/com/vendingontime/backend/routes/SignUpRoute.java
+++ src/main/java/com/vendingontime/backend/routes/SignUpRoute.java
@@ -1,8 +1,52 @@
 package com.vendingontime.backend.routes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vendingontime.backend.models.Person;
+import com.vendingontime.backend.models.PersonCollisionException;
+import com.vendingontime.backend.models.bodymodels.SignUpData;
+import com.vendingontime.backend.repositories.PersonRepository;
+import com.vendingontime.backend.routes.utils.AppRoute;
+import com.vendingontime.backend.routes.utils.Response;
+
 /**
  * Created by miguel on 13/3/17.
  */
 public class SignUpRoute {
+    private final ObjectMapper mapper = new ObjectMapper();
+    private PersonRepository repository;
+    private Response response;
 
+    public SignUpRoute(PersonRepository repository, Response response) {
+        this.repository = repository;
+        this.response = response;
+    }
+
+    public AppRoute post(String requestBody) {
+        try {
+            SignUpData personCandidate = mapper.readValue(requestBody, SignUpData.class);
+
+            return createUser(personCandidate);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            //FIXME
+            return null;
+        }
+    }
+
+    private AppRoute createUser(SignUpData personCandidate) {
+        String[] signUpErrors = personCandidate.validate();
+        if(signUpErrors.length != 0) {
+            response.badRequest(signUpErrors);
+        }
+
+        Person person = new Person(personCandidate);
+
+        try {
+            repository.create(person);
+        } catch (PersonCollisionException ex) {
+            response.badRequest(ex.getCauses());
+        }
+
+        return response.created(person);
+    }
 }
--- src/main/java/com/vendingontime/backend/routes/utils/HttpResponseFactory.java
+++ src/main/java/com/vendingontime/backend/routes/utils/HttpResponseFactory.java
@@ -4,9 +4,18 @@ package com.vendingontime.backend.routes.utils;
  * Created by Alberto on 13/03/2017.
  */
 public class HttpResponseFactory {
+    private volatile static Response response;
     private static final String APPLICATION_JSON = "application/json";
 
-    public static HttpResponse json() {
-        return new HttpResponse(APPLICATION_JSON, new JSONTransformer(), new RESTResultFactory());
+    public static Response json() {
+        if( response == null) {
+            synchronized (HttpResponseFactory.class) {
+                if (response == null) {
+                    response = new HttpResponse(APPLICATION_JSON, new JSONTransformer(), new RESTResultFactory());
+                }
+            }
+        }
+
+        return response;
     }
 }
