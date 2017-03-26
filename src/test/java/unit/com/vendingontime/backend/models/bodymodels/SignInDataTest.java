40a98f3b85247741be6723960467e248b42a2d46
Miguel Salas <miguelsalasalegre@gmail.com>
2017-03-26 21:32:13 +0200

    Added and tested SignInData

---
+++ src/test/java/unit/com/vendingontime/backend/models/bodymodels/SignInDataTest.java
@@ -0,0 +1,53 @@
+package unit.com.vendingontime.backend.models.bodymodels;
+
+import com.vendingontime.backend.models.bodymodels.SignInData;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static com.vendingontime.backend.models.bodymodels.SignInData.*;
+
+/**
+ * Created by miguel on 3/26/17.
+ */
+public class SignInDataTest {
+    public SignInData user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new SignInData();
+
+        user.setEmail("user@example.com");
+        user.setPassword("12345");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        user = null;
+    }
+
+    @Test
+    public void validate_user_noErrors() {
+        assertArrayEquals(new String[]{}, user.validate());
+    }
+
+    @Test
+    public void validate_invalidMail_error() throws Exception {
+        user.setEmail("aaaa");
+        assertArrayEquals(new String[]{BAD_LOGIN}, user.validate());
+    }
+
+    @Test
+    public void validate_invalidPassword_error() throws Exception {
+        user.setPassword("");
+        assertArrayEquals(new String[]{BAD_LOGIN}, user.validate());
+    }
+
+    @Test
+    public void validate_user_allErrors() throws Exception {
+        user.setEmail("aaaa").setPassword("");
+        assertArrayEquals(new String[]{BAD_LOGIN}, user.validate());
+    }
+
+}
