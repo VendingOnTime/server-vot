967f62ca62c00869a5ce11094cdb3a9b08aab579
Miguel Salas <al286275@uji.es>
2017-03-30 22:03:00 +0200

    Changed file template

--- src/main/java/com/vendingontime/backend/services/SignUpRoute.java
+++ src/main/java/com/vendingontime/backend/services/SignUpRoute.java
@@ -8,15 +8,27 @@ import com.vendingontime.backend.models.bodymodels.person.SignUpData;
 import com.vendingontime.backend.repositories.PersonRepository;
 import com.vendingontime.backend.routes.utils.AppRoute;
 
-import com.vendingontime.backend.routes.utils.HttpResponse;
 import com.vendingontime.backend.routes.utils.ServiceResponse;
 import com.vendingontime.backend.services.utils.BusinessLogicException;
 
-import javax.xml.ws.Service;
 import java.io.IOException;
 
-/**
- * Created by miguel on 13/3/17.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class SignUpRoute {
     public final static String MALFORMED_JSON = "MALFORMED_JSON";
