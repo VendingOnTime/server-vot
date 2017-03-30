08889c5f6d52bdb7c374c276aa47ea089447084a
Miguel Salas <al286275@uji.es>
2017-03-30 22:03:00 +0200

    Changed file template

--- src/main/java/com/vendingontime/backend/services/LogInService.java
+++ src/main/java/com/vendingontime/backend/services/LogInService.java
@@ -16,8 +16,22 @@ import java.util.Optional;
 
 import static com.vendingontime.backend.models.bodymodels.person.LogInData.BAD_LOGIN;
 
-/**
- * Created by miguel on 3/26/17.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 public class LogInService {
     public final static String MALFORMED_JSON = "MALFORMED_JSON";
