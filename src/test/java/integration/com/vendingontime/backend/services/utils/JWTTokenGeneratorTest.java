749a81d3c1bf2d6991a00617453067705df5a115
Alberto Gonzalez Perez <algonzal@uji.es>
2017-04-06 01:00:09 +0200

    Adding JWTTokenGenerator integration test

---
+++ src/test/java/integration/com/vendingontime/backend/services/utils/JWTTokenGeneratorTest.java
@@ -0,0 +1,87 @@
+package integration.com.vendingontime.backend.services.utils;
+
+import com.vendingontime.backend.models.bodymodels.person.LogInData;
+import com.vendingontime.backend.models.bodymodels.person.SignUpData;
+import com.vendingontime.backend.models.person.Person;
+import com.vendingontime.backend.repositories.PersonRepository;
+import com.vendingontime.backend.services.SignUpService;
+import com.vendingontime.backend.services.utils.JWTTokenGenerator;
+import integration.com.vendingontime.backend.repositories.testutils.IntegrationTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+import java.util.Optional;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+public class JWTTokenGeneratorTest extends IntegrationTest {
+
+    private static final String EMAIL = "supervisor@example.com";
+    private static final String USERNAME = "supervisor1";
+    private static final String PASSWORD = "password";
+
+    @Inject
+    private JWTTokenGenerator tokenGenerator;
+
+    @Inject
+    private SignUpService service;
+
+    @Inject
+    private PersonRepository repository;
+
+    private SignUpData signUpData;
+    private LogInData logInData;
+
+    @Before
+    public void setUp() throws Exception {
+        signUpData = new SignUpData()
+                .setUsername(USERNAME)
+                .setEmail(EMAIL)
+                .setPassword(PASSWORD);
+
+        logInData = new LogInData()
+                .setEmail(EMAIL)
+                .setPassword(PASSWORD);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        signUpData = null;
+        logInData = null;
+    }
+
+    @Test
+    public void recoverFrom() throws Exception {
+        String token = tokenGenerator.generateFrom(logInData);
+        Person supervisor = service.createSupervisor(signUpData);
+
+        Optional<Person> possiblePerson = tokenGenerator.recoverFrom(token);
+        assertTrue(possiblePerson.isPresent());
+        assertThat(possiblePerson.get(), equalTo(supervisor));
+
+        repository.delete(supervisor.getId());
+    }
+
+}
