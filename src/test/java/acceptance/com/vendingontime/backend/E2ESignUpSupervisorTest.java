7fca6b407190744726f254220d58e8d6d9d472d0
Miguel Salas <al286275@uji.es>
2017-04-30 12:16:13 +0200

    Added new tests to E2ESignUpSupervisorTest

--- src/test/java/acceptance/com/vendingontime/backend/E2ESignUpSupervisorTest.java
+++ src/test/java/acceptance/com/vendingontime/backend/E2ESignUpSupervisorTest.java
@@ -14,8 +14,11 @@ import javax.inject.Inject;
 
 import java.util.Optional;
 
+import static com.vendingontime.backend.models.bodymodels.person.SignUpData.INVALID_EMAIL;
+import static com.vendingontime.backend.routes.AbstractSparkRouter.MALFORMED_JSON;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -41,27 +44,58 @@ public class E2ESignUpSupervisorTest extends E2ETest {
     @Inject private PersonRepository repository;
 
     @Test
-    public void createSupervisor() throws Exception {
+    public void createSupervisor_withValidJSON_andValidData_returnsSupervisorData() throws Exception {
         SignUpData payload = FixtureFactory.generateSignUpData();
 
         given()
-                .body(payload)
+            .body(payload)
         .when()
-                .post(host + SignUpRouter.V1_SIGN_UP_SUPERVISOR)
+            .post(host + SignUpRouter.V1_SIGN_UP_SUPERVISOR)
         .then()
-                .statusCode(HttpResponse.StatusCode.CREATED)
-                .body("success", is(true))
-                .body("data.dni", equalTo(payload.getDni()))
-                .body("data.username", equalTo(payload.getUsername()))
-                .body("data.email", equalTo(payload.getEmail()))
-                .body("data.name", equalTo(payload.getName()))
-                .body("data.surnames", equalTo(payload.getSurnames()))
-                .body("data.password", nullValue())
-                .body("data.role", equalTo(PersonRole.SUPERVISOR.toString().toLowerCase()))
-                .body("error", nullValue());
+            .statusCode(HttpResponse.StatusCode.CREATED)
+            .body("success", is(true))
+            .body("data.dni", equalTo(payload.getDni()))
+            .body("data.username", equalTo(payload.getUsername()))
+            .body("data.email", equalTo(payload.getEmail()))
+            .body("data.name", equalTo(payload.getName()))
+            .body("data.surnames", equalTo(payload.getSurnames()))
+            .body("data.password", nullValue())
+            .body("data.role", equalTo(PersonRole.SUPERVISOR.toString().toLowerCase()))
+            .body("error", nullValue());
 
-        Optional<Person> byEmail = repository.findByEmail(payload.getEmail());
-        repository.delete(byEmail.get().getId());
+        repository.deleteAll();
     }
 
+    @Test
+    public void createSupervisor_withValidJSON_andInvalidData_returnsBadRequest() throws Exception {
+        SignUpData payload = FixtureFactory.generateSignUpData()
+                .setEmail("invalidEmail");
+
+        given()
+            .body(payload)
+        .when()
+            .post(host + SignUpRouter.V1_SIGN_UP_SUPERVISOR)
+        .then()
+            .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+            .body("success", is(false))
+            .body("data", nullValue())
+            .body("error", contains(INVALID_EMAIL));
+
+        repository.deleteAll();
+    }
+
+    @Test
+    public void createSupervisor_withInvalidJSON_returnsMalformedJSON() throws Exception {
+        given()
+            .body("")
+        .when()
+            .post(host + SignUpRouter.V1_SIGN_UP_SUPERVISOR)
+        .then()
+            .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+            .body("success", is(false))
+            .body("data", nullValue())
+            .body("error", is(MALFORMED_JSON));
+
+        repository.deleteAll();
+    }
 }
