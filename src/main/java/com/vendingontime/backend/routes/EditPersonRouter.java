a450ec5274a522f711dda6afa556b87f03d3d62f
Alberto Gonzalez Perez <algonzal@uji.es>
2017-05-04 19:28:56 +0200

    EditPersonRouter created

---
+++ src/main/java/com/vendingontime/backend/routes/EditPersonRouter.java
@@ -0,0 +1,69 @@
+package com.vendingontime.backend.routes;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+import com.google.inject.Inject;
+import com.vendingontime.backend.middleware.EndpointProtector;
+import com.vendingontime.backend.middleware.TokenEndpointProtector;
+import com.vendingontime.backend.models.bodymodels.person.EditPersonData;
+import com.vendingontime.backend.models.person.Person;
+import com.vendingontime.backend.routes.utils.AppRoute;
+import com.vendingontime.backend.routes.utils.ServiceResponse;
+import com.vendingontime.backend.services.EditPersonService;
+import com.vendingontime.backend.services.utils.BusinessLogicException;
+import spark.Service;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class EditPersonRouter extends AbstractSparkRouter {
+
+    public static final String V1_EDIT_PROFILE = V1 + "/users/profile/";
+
+    private final EditPersonService service;
+    private final EndpointProtector protector;
+
+    @Inject
+    public EditPersonRouter(ServiceResponse serviceResponse, EditPersonService service, EndpointProtector protector) {
+        super(serviceResponse);
+        this.service = service;
+        this.protector = protector;
+    }
+
+    @Override
+    public void configure(Service http) {
+        protector.protect(V1_EDIT_PROFILE + ID_PARAM);
+        http.put(V1_EDIT_PROFILE + ID_PARAM, map((req, res) ->
+                editProfile(req.params(ID_PARAM), req.body(), req.attribute(TokenEndpointProtector.LOGGED_IN_PERSON))));
+    }
+
+    public AppRoute editProfile(String id, String body, Person requester) {
+        try {
+            EditPersonData editPersonData = mapper.readValue(body, EditPersonData.class);
+            editPersonData.setId(id);
+            editPersonData.setRequester(requester);
+
+            Optional<Person> possiblePerson = service.updatePerson(editPersonData);
+            return possiblePerson.map(serviceResponse::ok).orElseGet(serviceResponse::notFound);
+        } catch (BusinessLogicException e) {
+            return serviceResponse.badRequest(e.getCauses());
+        } catch (IOException e) {
+            return serviceResponse.badRequest(MALFORMED_JSON);
+        }
+    }
+}
---
+++ src/test/java/unit/com/vendingontime/backend/routes/EditPersonRouterTest.java
@@ -0,0 +1,135 @@
+package unit.com.vendingontime.backend.routes;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vendingontime.backend.middleware.EndpointProtector;
+import com.vendingontime.backend.models.bodymodels.person.EditPersonData;
+import com.vendingontime.backend.models.person.Person;
+import com.vendingontime.backend.routes.EditPersonRouter;
+import com.vendingontime.backend.routes.utils.ServiceResponse;
+import com.vendingontime.backend.services.EditPersonService;
+import com.vendingontime.backend.services.utils.BusinessLogicException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import testutils.FixtureFactory;
+
+import java.util.Optional;
+
+import static com.vendingontime.backend.routes.AbstractSparkRouter.MALFORMED_JSON;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+public class EditPersonRouterTest {
+    private static final String PERSON_ID = "PERSON_ID";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private EditPersonService service;
+    private ServiceResponse serviceResponse;
+    private EndpointProtector protector;
+    private EditPersonRouter router;
+    private EditPersonData payload;
+    private Person person;
+    private Person requester;
+    private String stringifiedPerson;
+
+    @Before
+    public void setUp() throws Exception {
+        service = mock(EditPersonService.class);
+        serviceResponse = mock(ServiceResponse.class);
+        protector = mock(EndpointProtector.class);
+
+        router = new EditPersonRouter(serviceResponse, service, protector);
+
+        payload = FixtureFactory.generateEditPersonData();
+        requester = new Person(payload);
+        person = FixtureFactory.generateSupervisorWithCompany();
+        stringifiedPerson = objectMapper.writeValueAsString(payload);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        service = null;
+        serviceResponse = null;
+        protector = null;
+        router = null;
+        payload = null;
+        requester = null;
+        person = null;
+        stringifiedPerson = null;
+    }
+
+    @Test
+    public void editMachine_withValidData() {
+        when(service.updatePerson(any())).thenReturn(Optional.of(requester));
+
+        router.editProfile(PERSON_ID, stringifiedPerson, person);
+
+        verify(service, times(1))
+                .updatePerson((EditPersonData) payload.setId(PERSON_ID).setRequester(person));
+        verify(serviceResponse, times(1)).ok(requester);
+    }
+
+    @Test
+    public void editMachine_withInvalidData() {
+        String[] expectedErrors = new String[]{ EditPersonData.EMPTY_NAME };
+
+        doThrow(new BusinessLogicException(expectedErrors))
+                .when(service).updatePerson((EditPersonData) payload.setId(PERSON_ID).setRequester(person));
+        router.editProfile(PERSON_ID, stringifiedPerson, person);
+
+        verify(service, times(1)).updatePerson(payload.setId(PERSON_ID));
+        verify(serviceResponse, times(1)).badRequest(expectedErrors);
+    }
+
+    @Test
+    public void editMachine_withNotExistingMachine_returnsNotFound() throws Exception {
+        when(service.updatePerson(payload)).thenReturn(Optional.empty());
+
+        router.editProfile(PERSON_ID, stringifiedPerson, person);
+
+        verify(serviceResponse, times(1)).notFound();
+    }
+
+    @Test
+    public void editMachine_withEmptyJSON() {
+        stringifiedPerson = "";
+
+        router.editProfile(PERSON_ID, stringifiedPerson, person);
+
+        verify(serviceResponse, never()).ok(any());
+        verify(serviceResponse, times(1)).badRequest(MALFORMED_JSON);
+        verify(service, never()).updatePerson(any());
+    }
+
+    @Test
+    public void editMachine_withInvalidJSONField() {
+        stringifiedPerson = "{\"invalid\":\"1234\"}";
+
+        router.editProfile(PERSON_ID, stringifiedPerson, person);
+
+        verify(serviceResponse, never()).ok(any());
+        verify(serviceResponse, times(1)).badRequest(MALFORMED_JSON);
+        verify(service, never()).updatePerson(any());
+    }
+}
