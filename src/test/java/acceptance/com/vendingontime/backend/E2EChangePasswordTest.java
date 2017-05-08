8a3b6982ca9f8e4ad34ae91f7156dd802fd5379c
Alberto Gonzalez Perez <algonzal@uji.es>
2017-05-08 16:58:07 +0200

    Adding ChangePassword remaining acceptance tests

--- src/test/java/acceptance/com/vendingontime/backend/E2EChangePasswordTest.java
+++ src/test/java/acceptance/com/vendingontime/backend/E2EChangePasswordTest.java
@@ -19,10 +19,12 @@ package acceptance.com.vendingontime.backend;
 
 import acceptance.com.vendingontime.backend.testutils.E2ETest;
 import com.vendingontime.backend.models.bodymodels.person.EditPasswordData;
+import com.vendingontime.backend.models.bodymodels.person.LogInData;
 import com.vendingontime.backend.models.person.Person;
 import com.vendingontime.backend.models.person.PersonRole;
 import com.vendingontime.backend.repositories.PersonRepository;
 import com.vendingontime.backend.routes.EditPasswordRouter;
+import com.vendingontime.backend.routes.LogInRouter;
 import com.vendingontime.backend.routes.utils.HttpResponse;
 import com.vendingontime.backend.services.LogInService;
 import com.vendingontime.backend.services.SignUpService;
@@ -31,8 +33,12 @@ import testutils.FixtureFactory;
 
 import javax.inject.Inject;
 
+import static com.vendingontime.backend.models.bodymodels.person.EditPasswordData.EMPTY_NEW_PASSWORD;
+import static com.vendingontime.backend.routes.AbstractSparkRouter.MALFORMED_JSON;
+import static com.vendingontime.backend.services.AbstractService.INSUFFICIENT_PERMISSIONS;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -53,7 +59,6 @@ public class E2EChangePasswordTest extends E2ETest {
 
         String newPassword = "NEW_PASSWORD";
         payload.setNewPassword(newPassword);
-        payload.setId(null);
 
         given()
                 .body(payload)
@@ -72,11 +77,87 @@ public class E2EChangePasswordTest extends E2ETest {
                 .body("data.role", equalTo(PersonRole.SUPERVISOR.toString().toLowerCase()))
                 .body("error", nullValue());
 
-        // FIXME: alberto@7/5/17 This does not work because supervisor object is cached
-        //                       tested manually password is changed correctly
-
-        // loginService.authorizeUser(new LogInData().setEmail(supervisor.getEmail()).setPassword(newPassword));
+        LogInData newLogInData = new LogInData().setEmail(supervisor.getEmail()).setPassword(newPassword);
+        given()
+                .body(newLogInData)
+        .when()
+                .post(host + LogInRouter.V1_LOG_IN)
+        .then()
+                .statusCode(HttpResponse.StatusCode.OK)
+                .body("success", is(true));
 
         repository.delete(supervisor.getId());
+    }
+
+    @Test
+    // For a full list of all possible errors for invalid data, go to EditPasswordData's validate method.
+    public void changeSupervisorPassword_withValidJSON_andInvalidData_returnsBadRequest() throws Exception {
+        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+
+        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor));
+
+        EditPasswordData payload = FixtureFactory.generateEditPasswordDataFrom(supervisor);
+
+        payload.setNewPassword("");
+
+        given()
+                .body(payload)
+                .header("Authorization", "JWT " + token)
+        .when()
+                .put(host + EditPasswordRouter.V1_EDIT_PASSWORD + supervisor.getId())
+        .then()
+                .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+                .body("success", is(false))
+                .body("data", nullValue())
+                .body("error", contains(EMPTY_NEW_PASSWORD));
+
+        repository.delete(supervisor.getId());
+    }
+
+    @Test
+    public void editSupervisor_withInvalidJSON_returnsMalformedJSON() throws Exception {
+        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+
+        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor));
+
+        given()
+                .body("")
+                .header("Authorization", "JWT " + token)
+        .when()
+                .put(host + EditPasswordRouter.V1_EDIT_PASSWORD+ supervisor.getId())
+        .then()
+                .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+                .body("success", is(false))
+                .body("data", nullValue())
+                .body("error", is(MALFORMED_JSON));
+
+        repository.deleteAll();
+    }
+
+    @Test
+    public void editSupervisor_withUnauthorizedUser_returnsInsufficientPermissions() throws Exception {
+        Person supervisor1 = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person supervisor2 = signUpService.createSupervisor(FixtureFactory.generateSignUpData()
+                .setDni("12345678W").setEmail("another.email@example.com").setUsername("supervisor2"));
+
+        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor2));
+
+        EditPasswordData payload = FixtureFactory.generateEditPasswordDataFrom(supervisor1);
+
+        String newPassword = "NEW_PASSWORD";
+        payload.setNewPassword(newPassword);
+
+        given()
+                .body(payload)
+                .header("Authorization", "JWT " + token)
+        .when()
+                .put(host + EditPasswordRouter.V1_EDIT_PASSWORD+ supervisor1.getId())
+        .then()
+                .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+                .body("success", is(false))
+                .body("data", nullValue())
+                .body("error", contains(INSUFFICIENT_PERMISSIONS));
+
+        repository.deleteAll();
     }
 }
