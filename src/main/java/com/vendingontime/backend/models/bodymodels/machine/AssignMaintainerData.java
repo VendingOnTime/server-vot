b7f4bfa52b002e1c070e63d0c4c64d6093db1cd1
Alberto Gonzalez Perez <algonzal@uji.es>
2017-05-12 13:01:24 +0200

    Updating license comment

--- src/main/java/com/vendingontime/backend/models/bodymodels/machine/AssignMaintainerData.java
+++ src/main/java/com/vendingontime/backend/models/bodymodels/machine/AssignMaintainerData.java
@@ -7,11 +7,25 @@ import java.util.LinkedList;
 
 import static com.vendingontime.backend.utils.StringUtils.isEmpty;
 
-/**
- * Created by Alberto on 11/05/2017.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
-// FIXME: 11/05/2017 All this bodymodels should be refactored, some of them share common fields that could be abstracted
+// FIXME: 11/05/2017 All these bodymodels should be refactored, some of them share common fields that could be abstracted
 public class AssignMaintainerData implements Validable {
 
     public static final String EMPTY_MACHINE_ID = "EMPTY_MACHINE_ID";
