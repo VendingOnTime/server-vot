4e24fe22887a8945e3b7ba5e4861a03b62fea4b8
Alberto Gonzalez <al286290@uji.es>
2017-05-13 18:58:30 +0200

    RemoveMachineRouter created and tested

--- src/main/java/com/vendingontime/backend/routes/RemoveTechnicianRouter.java
+++ src/main/java/com/vendingontime/backend/routes/RemoveTechnicianRouter.java
@@ -1,5 +1,11 @@
 package com.vendingontime.backend.routes;
-/*
+
+import com.google.inject.Inject;
+import com.vendingontime.backend.middleware.EndpointProtector;
+import com.vendingontime.backend.routes.utils.ServiceResponse;
+import com.vendingontime.backend.services.AbstractRemoveService;
+
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -7,28 +13,21 @@ package com.vendingontime.backend.routes;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+public class RemoveTechnicianRouter extends AbstractRemoveRouter {
 
-import com.google.inject.Inject;
-import com.vendingontime.backend.middleware.EndpointProtector;
-import com.vendingontime.backend.routes.utils.ServiceResponse;
-import com.vendingontime.backend.services.RemoveMachineService;
-
-public class RemoveMachineRouter extends AbstractRemoveRouter {
-
-    public static final String V1_REMOVE_MACHINE = V1 + "/machines/";
+    public static final String V1_REMOVE_TECHNICIAN = V1 + "/technicians/";
 
     @Inject
-    public RemoveMachineRouter(ServiceResponse serviceResponse, EndpointProtector protector,
-                               RemoveMachineService service) {
-        super(serviceResponse, protector, service, V1_REMOVE_MACHINE);
+    public RemoveTechnicianRouter(ServiceResponse serviceResponse, EndpointProtector protector, AbstractRemoveService service) {
+        super(serviceResponse, protector, service, V1_REMOVE_TECHNICIAN);
     }
 }
