0f3d068cc854c64dace5f8302cc9f089ee52c4b6
Alberto Gonzalez <al286290@uji.es>
2017-05-14 18:21:50 +0200

    VOT-15 Now supervisors can modify the data of their own technicians

--- src/test/java/acceptance/com/vendingontime/backend/E2EEditSupervisorDataTest.java
+++ src/test/java/acceptance/com/vendingontime/backend/E2EEditSupervisorDataTest.java
@@ -24,10 +24,10 @@ import com.vendingontime.backend.models.person.Person;
 import com.vendingontime.backend.models.person.PersonRole;
 import com.vendingontime.backend.repositories.PersonRepository;
 import com.vendingontime.backend.routes.EditPersonRouter;
-import com.vendingontime.backend.routes.SignUpRouter;
 import com.vendingontime.backend.routes.utils.HttpResponse;
 import com.vendingontime.backend.services.LogInService;
 import com.vendingontime.backend.services.SignUpService;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import testutils.FixtureFactory;
 
@@ -45,55 +45,86 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class E2EEditSupervisorDataTest extends E2ETest {
 
     @Inject private SignUpService signUpService;
-    @Inject private LogInService loginService;
+    @Inject private LogInService logInService;
 
     @Inject private PersonRepository repository;
 
     @Test
-    public void editSupervisor_withValidJSON_andValidData_returnsUpdatedSupervisorData() throws Exception {
-        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+    public void supervisorEditTechnician_withValidJSON_andValidData_returnsUpdatedTechnicianData() throws Exception {
+        Person requester = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person technician = signUpService.createTechnician(FixtureFactory.generateAddTechnicianData().setRequester(requester));
 
-        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor));
+        String token = logInService.authorizeUser(FixtureFactory.generateLogInDataFrom(requester));
 
-        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(supervisor);
-
+        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(technician);
         payload.setName("NEW_NAME");
 
         given()
                 .body(payload)
                 .header("Authorization", "JWT " + token)
         .when()
-                .put(host + EditPersonRouter.V1_EDIT_PROFILE + supervisor.getId())
+                .put(host + EditPersonRouter.V1_EDIT_PROFILE + technician.getId())
         .then()
                 .statusCode(HttpResponse.StatusCode.OK)
                 .body("success", is(true))
-                .body("data.dni", equalTo(supervisor.getDni()))
-                .body("data.username", equalTo(supervisor.getUsername()))
-                .body("data.email", equalTo(supervisor.getEmail()))
+                .body("data.dni", equalTo(technician.getDni()))
+                .body("data.username", equalTo(technician.getUsername()))
+                .body("data.email", equalTo(technician.getEmail()))
                 .body("data.name", equalTo(payload.getName()))
-                .body("data.surnames", equalTo(supervisor.getSurnames()))
+                .body("data.surnames", equalTo(technician.getSurnames()))
                 .body("data.password", nullValue())
-                .body("data.role", equalTo(PersonRole.SUPERVISOR.toString().toLowerCase()))
+                .body("data.role", equalTo(PersonRole.TECHNICIAN.toValue()))
                 .body("error", nullValue());
 
-        repository.delete(supervisor.getId());
+        repository.deleteAll();
+    }
+
+    @Test
+    public void removeTechnician_differentCompanies_outOfMyProperty() throws Exception {
+        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person technician = signUpService.createTechnician(FixtureFactory.generateAddTechnicianData().setRequester(supervisor));
+
+        SignUpData anotherSupervisorData = FixtureFactory.generateSignUpData()
+                .setUsername("ANOTHER_USERNAME")
+                .setEmail("another.email@example.com")
+                .setDni("22222222Z");
+        Person requester = signUpService.createSupervisor(anotherSupervisorData);
+
+        String token = logInService.authorizeUser(FixtureFactory.generateLogInDataFrom(requester));
+
+        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(technician);
+        payload.setName("NEW_NAME");
+
+        given() // a competitor
+                .body(payload)
+                .header("Authorization", "JWT " + token)
+        .when() // performs unfair competition
+                .put(host + EditPersonRouter.V1_EDIT_PROFILE + technician.getId())
+        .then() // covert operation cancelled
+                .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
+                .body("success", is(false))
+                .body("data", Matchers.nullValue())
+                .body("error", Matchers.contains(INSUFFICIENT_PERMISSIONS));
+
+        repository.deleteAll();
     }
 
     @Test
     //It also throws SignUpData's validation errors
-    public void editSupervisor_withValidJSON_andInvalidData_returnsBadRequest() throws Exception {
-        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+    public void supervisorEditTechnician_withValidJSON_andInvalidData_returnsBadRequest() throws Exception {
+        Person requester = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person technician = signUpService.createTechnician(FixtureFactory.generateAddTechnicianData().setRequester(requester));
 
-        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor));
+        String token = logInService.authorizeUser(FixtureFactory.generateLogInDataFrom(requester));
 
-        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(supervisor);
+        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(technician);
         payload.setEmail("invalidEmail");
 
         given()
                 .body(payload)
                 .header("Authorization", "JWT " + token)
         .when()
-                .put(host + EditPersonRouter.V1_EDIT_PROFILE + supervisor.getId())
+                .put(host + EditPersonRouter.V1_EDIT_PROFILE + technician.getId())
         .then()
                 .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
                 .body("success", is(false))
@@ -104,45 +135,22 @@ public class E2EEditSupervisorDataTest extends E2ETest {
     }
 
     @Test
-    public void editSupervisor_withInvalidJSON_returnsMalformedJSON() throws Exception {
-        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+    public void supervisorEditTechnician_withInvalidJSON_returnsMalformedJSON() throws Exception {
+        Person requester = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person technician = signUpService.createTechnician(FixtureFactory.generateAddTechnicianData().setRequester(requester));
 
-        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor));
+        String token = logInService.authorizeUser(FixtureFactory.generateLogInDataFrom(requester));
 
         given()
                 .body("")
                 .header("Authorization", "JWT " + token)
         .when()
-                .put(host + EditPersonRouter.V1_EDIT_PROFILE + supervisor.getId())
+                .put(host + EditPersonRouter.V1_EDIT_PROFILE + technician.getId())
         .then()
                 .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
                 .body("success", is(false))
                 .body("data", nullValue())
                 .body("error", is(MALFORMED_JSON));
-
-        repository.deleteAll();
-    }
-
-    @Test
-    public void editSupervisor_withUnauthorizedUser_returnsInsufficientPermissions() throws Exception {
-        Person supervisor1 = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
-        Person supervisor2 = signUpService.createSupervisor(FixtureFactory.generateSignUpData()
-                .setDni("12345678W").setEmail("another.email@example.com").setUsername("supervisor2"));
-
-        String token = loginService.authorizeUser(FixtureFactory.generateLogInDataFrom(supervisor2));
-
-        EditPersonData payload = FixtureFactory.generateEditPersonDataFrom(supervisor1);
-
-        given()
-                .body(payload)
-                .header("Authorization", "JWT " + token)
-        .when()
-                .put(host + EditPersonRouter.V1_EDIT_PROFILE + supervisor1.getId())
-        .then()
-                .statusCode(HttpResponse.StatusCode.BAD_REQUEST)
-                .body("success", is(false))
-                .body("data", nullValue())
-                .body("error", contains(INSUFFICIENT_PERMISSIONS));
 
         repository.deleteAll();
     }
