e1fa760c0ffb7885aa2659c811bb0a9b324dde78
Alberto Gonzalez <al286290@uji.es>
2017-05-15 20:50:08 +0200

    VOT-153 GetPersonRouter integrated

--- src/test/java/integration/com/vendingontime/backend/routes/GetPersonRouterTest.java
+++ src/test/java/integration/com/vendingontime/backend/routes/GetPersonRouterTest.java
@@ -8,7 +8,9 @@ import com.vendingontime.backend.repositories.CompanyRepository;
 import com.vendingontime.backend.repositories.MachineRepository;
 import com.vendingontime.backend.repositories.PersonRepository;
 import com.vendingontime.backend.routes.GetMachineRouter;
+import com.vendingontime.backend.routes.GetPersonRouter;
 import com.vendingontime.backend.routes.utils.RESTResult;
+import com.vendingontime.backend.services.SignUpService;
 import integration.com.vendingontime.backend.testutils.IntegrationTest;
 import org.junit.Test;
 import spark.Request;
@@ -21,8 +23,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
-
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -30,36 +31,29 @@ import static org.mockito.Mockito.mock;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+public class GetPersonRouterTest extends IntegrationTest {
 
-public class GetMachineRouterTest extends IntegrationTest {
-
-    @Inject private GetMachineRouter router;
+    @Inject private SignUpService signUpService;
+    @Inject private GetPersonRouter router;
 
     @Inject private PersonRepository personRepository;
     @Inject private CompanyRepository companyRepository;
-    @Inject private MachineRepository machineRepository;
 
     @Test
     public void getMachine() throws Exception {
-        Company company = companyRepository.create(FixtureFactory.generateCompanyWithOwner());
-        Machine machine = machineRepository.create(FixtureFactory.generateMachine());
+        Person supervisor = signUpService.createSupervisor(FixtureFactory.generateSignUpData());
+        Person technician = signUpService.createTechnician(FixtureFactory.generateAddTechnicianData().setRequester(supervisor));
 
-        Person savedOwner = personRepository.findById(company.getOwner().getId()).get();
-        Machine savedMachine = machineRepository.findById(machine.getId()).get();
-
-        company.addMachine(savedMachine);
-        companyRepository.update(company);
-
-        String result = (String) router.getWith(savedMachine.getId(), savedOwner)
+        String result = (String) router.getWith(technician.getId(), supervisor)
                 .handle(mock(Request.class), mock(Response.class));
 
         ObjectMapper mapper = new ObjectMapper();
@@ -67,7 +61,6 @@ public class GetMachineRouterTest extends IntegrationTest {
         assertThat(restResult.getSuccess(), is(true));
         assertNotNull(restResult.getData());
 
-        machineRepository.deleteAll();
         personRepository.deleteAll();
         companyRepository.deleteAll();
     }
